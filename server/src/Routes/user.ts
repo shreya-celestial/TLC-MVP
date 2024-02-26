5e11e8a827b8566617a3a0324ab2ea0bc867e418
shreya-celestial <toddlercoding@gmail.com>
2024-02-26 17:18:40 +0530

    adding verify user functionality

--- server/src/Routes/user.ts
+++ server/src/Routes/user.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/userController";
+import signup from "../controllers/user/signup";
+import verify from "../controllers/user/verify";
 const router = express.Router();
 
 router.post('/signup', signup)
+router.get('/verify/:token', verify)
 
 export default router
