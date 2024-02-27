d27af364e49b32b880e18e897afd24ae0dd2d5a4
shreya-celestial <toddlercoding@gmail.com>
2024-02-27 12:58:49 +0530

    updating a few files

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import user from "./Routes/user"
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config()
 
 const app = express();
 app.use(express.json());
+app.use(cors())
+app.use(cookieParser());
 
 app.use('/user', user)
 
