0bf06c5910cf9a5b3ca5e8f5dc20ef25a53859f3
Anish Chaurasia <anish.chaurasia@CELSYSWTC.IN>
2024-03-02 12:11:28 +0530

    sidebar-header-design-change

--- client/src/Components/Wrapper/Wrapper.jsx
+++ client/src/Components/Wrapper/Wrapper.jsx
@@ -1,19 +1,20 @@
 import { Box } from '@mui/material';
 import Sidebar from '../Sidebar/Sidebar';
 import Navbar from '../Navbar/Navbar';
-
 import { useStyles } from './Wrapper.styles';
-
+import { useState } from 'react';
 const Wrapper = ({ children }) => {
+  const [open, setOpen] = useState(false);
   const classes = useStyles();
+  const handleSidebarOpen = () => {
+    setOpen(!open);
+  };
 
   return (
     <Box className={classes.root}>
-      <Navbar />
-      <Box className={classes.body}>
-        <Sidebar />
-        <Box className={classes.main}>{children}</Box>
-      </Box>
+      <Navbar handleSidebarOpen={handleSidebarOpen} />
+      <Sidebar open={open} />
+      <Box className={classes.main}>{children}</Box>
     </Box>
   );
 };
