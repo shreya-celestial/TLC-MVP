770986a5ddc8b01103a94b2aa33c877ce06df293
shreya-celestial <toddlercoding@gmail.com>
2024-03-14 10:09:33 +0530

    styles-for-dashboard-loader-fixed

--- client/src/Components/Loader/Loader.styles.js
+++ client/src/Components/Loader/Loader.styles.js
@@ -10,4 +10,16 @@ export const useStyles = makeStyles((theme) => ({
       },
     },
   },
+  dashboardLoader: {
+    '&.MuiBackdrop-root': {
+      height: 'calc(100vh - 64px)',
+      width: 'calc(100vw - 16%)',
+      marginTop: '64px',
+      marginLeft: '16%',
+      backgroundColor: '#FFFFFF',
+      '& svg': {
+        color: '#259311',
+      },
+    },
+  },
 }));
