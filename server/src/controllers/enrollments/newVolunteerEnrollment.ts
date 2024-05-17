0e63aa38206f06a56ba593c31ca0b795fc9a8f90
shreya-celestialsys <shreya.celestialsys@gmail.com>
2024-05-17 17:25:01 +0530

    api-for-creating-new-enrollment-using-volunteers

---
+++ server/src/controllers/enrollments/newVolunteerEnrollment.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express"
+import { capitaliseStr, formatDate } from "../../utils/global"
+import getData from "../../utils/getData"
+import { addVolunteerEnrollment } from "../../gql/enrollments/mutations"
+
+const newVolunteerEnrollment = async (req: Request, res: Response) => {
+  const children = req?.body?.children?.map((child: any)=>{
+    return {
+      dob: formatDate(child.dob), 
+      gender: child.gender, 
+      name: capitaliseStr(child.name)
+    }
+  })
+
+  const variables = {
+    ...req?.body,
+    enrolled_by: req?.body?.enrolled_by?.toLowerCase(),
+    state: capitaliseStr(req?.body?.state),
+    name: capitaliseStr(req?.body?.name),
+    email: req?.body?.email?.toLowerCase(),
+    children
+  }
+
+  const data = await getData(addVolunteerEnrollment, variables)
+  if(data?.errors)
+  {
+    return res.status(400).json({
+      status: 'error',
+      message: data?.errors[0]?.message
+    })
+  }
+
+  if(data?.data?.insert_enrollments?.affected_rows)
+  {
+    return res.status(200).json({
+      status: 'success',
+      message: 'Enrollment inserted successfully'
+    })
+  }
+
+  return res.status(400).json({
+    status: 'error',
+    message: 'Something went wrong. Please try again later!'
+  })
+
+}
+
+export default newVolunteerEnrollment
