4c4e6cb2dbf92e614293ee4fcfd2c0e0a5a38c44
shreya-celestialsys <shreya.celestialsys@gmail.com>
2024-05-20 10:32:22 +0530

    meetings-not-required-on-update-bug-fixed

--- server/dist/controllers/workshops/updateWorkshop.js
+++ server/dist/controllers/workshops/updateWorkshop.js
@@ -16,7 +16,7 @@ const global_1 = require("../../utils/global");
 const getData_1 = __importDefault(require("../../utils/getData"));
 const mutations_1 = require("../../gql/workshops/mutations");
 const updateWorkshop = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b, _c, _d, _e, _f, _g, _h, _j;
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l;
     const vols = req.body.vols.map((vol) => {
         var _a;
         return {
@@ -60,7 +60,13 @@ const updateWorkshop = (req, res) => __awaiter(void 0, void 0, void 0, function*
             message: (_c = data === null || data === void 0 ? void 0 : data.errors[0]) === null || _c === void 0 ? void 0 : _c.message
         });
     }
-    const meeting = (_e = (_d = req === null || req === void 0 ? void 0 : req.body) === null || _d === void 0 ? void 0 : _d.meetings) === null || _e === void 0 ? void 0 : _e.map((meeting) => {
+    if (!((_e = (_d = req === null || req === void 0 ? void 0 : req.body) === null || _d === void 0 ? void 0 : _d.meetings) === null || _e === void 0 ? void 0 : _e.length)) {
+        return res.status(200).json({
+            status: 'success',
+            message: 'Workshop updated successfully!'
+        });
+    }
+    const meeting = (_g = (_f = req === null || req === void 0 ? void 0 : req.body) === null || _f === void 0 ? void 0 : _f.meetings) === null || _g === void 0 ? void 0 : _g.map((meeting) => {
         return {
             id: {
                 _eq: meeting
@@ -69,15 +75,15 @@ const updateWorkshop = (req, res) => __awaiter(void 0, void 0, void 0, function*
     });
     const meetingData = yield (0, getData_1.default)(mutations_1.updateWorkshopMeetings, {
         meeting,
-        id: (_f = req === null || req === void 0 ? void 0 : req.params) === null || _f === void 0 ? void 0 : _f.id
+        id: (_h = req === null || req === void 0 ? void 0 : req.params) === null || _h === void 0 ? void 0 : _h.id
     });
     if (meetingData === null || meetingData === void 0 ? void 0 : meetingData.errors) {
         return res.status(400).json({
             status: 'error',
-            message: (_g = meetingData === null || meetingData === void 0 ? void 0 : meetingData.errors[0]) === null || _g === void 0 ? void 0 : _g.message
+            message: (_j = meetingData === null || meetingData === void 0 ? void 0 : meetingData.errors[0]) === null || _j === void 0 ? void 0 : _j.message
         });
     }
-    if ((_j = (_h = meetingData === null || meetingData === void 0 ? void 0 : meetingData.data) === null || _h === void 0 ? void 0 : _h.update_meetings) === null || _j === void 0 ? void 0 : _j.affected_rows) {
+    if ((_l = (_k = meetingData === null || meetingData === void 0 ? void 0 : meetingData.data) === null || _k === void 0 ? void 0 : _k.update_meetings) === null || _l === void 0 ? void 0 : _l.affected_rows) {
         return res.status(200).json({
             status: 'success',
             message: 'Workshop updated successfully!'
