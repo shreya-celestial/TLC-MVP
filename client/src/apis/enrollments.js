cfa6c42c0e0ae762146d6f534f649de85524a54c
gaurav-celestial <gaurav.yadav@celestialsys.com>
2025-02-12 11:44:49 +0530

    add link invitation functionality

--- client/src/apis/enrollments.js
+++ client/src/apis/enrollments.js
@@ -194,3 +194,28 @@ export const getLinkForEnrollInvite = async function ({ user }) {
   const resData = await res.json();
   return resData;
 };
+
+
+export const linkEnrol = async function ({ body }) {
+  console.log(body)
+  const res = await fetch(
+    `${BASE_URL}/linkEnrol`,
+    {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  if (!res.ok) {
+    const error = new Error('An error occured while fetching the data');
+    error.code = res.status;
+    error.info = await res.json();
+    throw error;
+  }
+
+  const resData = await res.json();
+  return resData;
+};
